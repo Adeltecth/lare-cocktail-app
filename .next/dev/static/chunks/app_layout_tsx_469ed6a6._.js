42192c3fb9c13afae6fe623408fba9afb3c686dd
Your Name <your_email@example.com>
2025-11-15 23:54:21 +0100

    Fix .gitignore and remove build files

--- .next/dev/static/chunks/app_layout_tsx_469ed6a6._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__493ef8ca._.css",
-  "static/chunks/node_modules_5dc386da._.js",
-  "static/chunks/components_bbb1403d._.js"
-],
-    source: "dynamic"
-});
